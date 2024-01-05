315fc51437b1443dc566c5e1930bd3690cfc5853
andrewye0128 <b0936486128@gmail.com>
2024-01-05 20:53:23 +0800

    add login and contact layout , and update content

--- page1/part3/assets/js/common.js
+++ page1/part3/assets/js/common.js
@@ -1,19 +1,38 @@
+// 共同JS
+// login & signup
+const loginPoint = document.querySelector("#loginClick");
+const loginPage = document.querySelector(".login-wrapper");
 
+loginPoint.addEventListener("click", () => {
+  loginPage.style.display = "flex";
+});
+
+document.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const target = e.target;
+  console.log(target);
+  if (target === loginPoint) {
+     loginPage.style.display = "flex";
+  } else if (target === loginPage) {
+    loginPage.style.display = "none";
+  } 
+});
+
+// loginPoint.addEventListener("click", () => {
+//   loginPage.style.display = "none";
+// });
 
 // Search display none or block function
 function searchBlock() {
-  let element = app.get(".search-section")
-  element.classList.toggle("d-block")
+  let element = app.get(".search-section");
+  element.classList.toggle("d-block");
 }
 
-
 // Click search Diaplay none or block
-app.get("#Search").addEventListener('click', searchBlock)
-
+app.get("#Search").addEventListener("click", searchBlock);
 
 // back to top
-app.get("#top").addEventListener('click', function () {
-  document.body.scrollTop = 0
-  document.documentElement.scrollTop
-})
-
+app.get("#top").addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop;
+});
